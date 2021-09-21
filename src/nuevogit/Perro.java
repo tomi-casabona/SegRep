d0439d4f654665b308be5e9b4c2475eab431efec
Tomas-Casabona <tomas.casabona@gmail.com>
2021-09-21 20:03:23 -0300

    primer commit desde tomi2 con perro

---
+++ src/nuevogit/Perro.java
@@ -0,0 +1,27 @@
+
+
+package nuevogit;
+
+/**
+ *
+ * @author Tomi
+ */
+public class Perro {
+    private String color;
+    private String raza;
+    private String pelo;
+    private String orejas;
+
+    public Perro() {
+    }
+
+    public Perro(String color, String raza, String pelo, String orejas) {
+        this.color = color;
+        this.raza = raza;
+        this.pelo = pelo;
+        this.orejas = orejas;
+    }
+    
+    
+
+}
